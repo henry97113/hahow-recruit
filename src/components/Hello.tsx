5382f2bc31173573cfe7315f6fe538bf2088fd9a
Henry <henry.chou@tuta.io>
2019-08-07 21:04:20 +0800

    project init
    Build development environment. The template comes from the one I built sometime before.

---
+++ src/components/Hello.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Hello = () => {
+  return (
+    <>
+      <h1>React & TypeScript Template</h1>
+      <div>Read README to learn how to use this template.</div>
+    </>
+  );
+};
+
+export default Hello;
