b6764542d333c0e59d7269e1a77b286986b2cf5a
Henry <henry.chou@tuta.io>
2019-08-08 19:09:10 +0800

    highlight hero card when clicked

--- src/components/Hero/HeroCard.tsx
+++ src/components/Hero/HeroCard.tsx
@@ -3,8 +3,15 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 const imgPlaceholder = require('images/image placeholder.png');
 
-const Card = styled.div`
-  border: 4px solid #718096;
+interface CardProps {
+  active?: boolean;
+}
+
+const Card = styled.div<CardProps>`
+  /* border: 4px solid #718096; */
+  border-width: 4px;
+  border-style: solid;
+  border-color: ${props => (props.active ? 'tomato' : '#718096')};
   border-radius: 4px;
 `;
 
@@ -20,12 +27,26 @@ interface HeroCardProps {
 }
 
 const HeroCard = (props: HeroCardProps) => {
+  const [active, setActive] = React.useState(false);
+  const oddEvent = (match, location) => {
+    if (match) {
+      setActive(match.isExact);
+      return true;
+    } else {
+      setActive(false);
+    }
+    return false;
+  };
   return (
     <NavLink
       to={`/heros/${props.id}`}
       className="block w-full md:w-2/4 mb-2 lg:w-1/4 px-1"
+      isActive={oddEvent}
     >
-      <Card className="p-8 flex flex-col justify-center items-center">
+      <Card
+        className="p-8 flex flex-col justify-center items-center"
+        active={active}
+      >
         <CardImage src={props.image || imgPlaceholder} />
         <h2 className="mt-12 text-2xl">{props.name}</h2>
       </Card>
