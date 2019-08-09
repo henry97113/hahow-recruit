961b703689504ecaf4714582693022a1038ceec1
Henry <henry.chou@tuta.io>
2019-08-09 16:39:23 +0800

    fix: heroes typo

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -8,9 +8,9 @@ interface RootProps {
 const Root = (props: RootProps) => {
   return (
     <div className="absolute inset-0 flex flex-col justify-center items-center">
-      <h1 className="text-4xl">SUPERHEROS!</h1>
+      <h1 className="text-4xl">SUPERHEROES!</h1>
       <Link to="/heros" replace className="block mb-12 text-blue-400 text-2xl">
-        Click me to see superheros!
+        Click me to see superheroes!
       </Link>
     </div>
   );
